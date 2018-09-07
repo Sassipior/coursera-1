0816cffc12052fc34a1ef719dd1a56deb054df9a
Sassipior <bas.laan@gmail.com>
2018-09-07 11:21:25 +0200

    assignment 4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 (function () {
-var names = ["Yaakov", "Jessica", "Jennifer", "Jason", "Sebastiaan", "Frank", "Ralf", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
